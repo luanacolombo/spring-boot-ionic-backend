e1f35cc7acc09a63cfb90378f29765c325f8c17e
Luana Colombo <luuana.colombo@gmail.com>
2022-09-28 19:57:55 -0300

    Projeto criado

---
+++ src/main/java/com/example/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.example.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) { //executar o programa
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
