8c5f4a7951d0c403b619f9c051e558994dd11537
Luana Colombo <luuana.colombo@gmail.com>
2022-12-28 08:43:16 -0300

    Validação customizada: email não repedito na inserção de cliente

--- src/main/java/com/example/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/example/cursomc/repositories/ClienteRepository.java
@@ -2,6 +2,7 @@ package com.example.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.cursomc.domain.Cliente;
 
@@ -9,5 +10,6 @@ import com.example.cursomc.domain.Cliente;
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> { //esse objeto será capaz de realizar 
 	//operações de acesso a dados, como busca, salvar, alterar, deletar. Várias operações de acesso a dados referente ao 
 	//objeto cliente
-
+	@Transactional(readOnly=true) //não necessita ser envolvida com uma transação de banco de dados, fazendo ela ficar mais rápida 
+	Cliente findByEmail(String email); //busca no banco de dados um cliente passando o e-mail como argumento
 }
--- src/main/java/com/example/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/example/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.cursomc.domain.Cliente;
 import com.example.cursomc.domain.enums.TipoCliente;
 import com.example.cursomc.dto.ClienteNewDTO;
+import com.example.cursomc.repositories.ClienteRepository;
 import com.example.cursomc.resources.exceptions.FieldMessage;
 import com.example.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo; //p/ testar se o e-mail ja existe
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) { //se o tipo do DTO for igual a pessoa juridica e se o cnpj não for válido 
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido")); //retorna a mensagem
 		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail()); //logica para testar se o e-mail já existe 
+			if (aux != null) { //se o aux for diferente de nulo significa que ele encontrou um registro no banco que tinha o e-mail
+				 list.add(new FieldMessage("email", "email já existente")); //se o e-mail já existe mostra o erro e a mensagem
+			}
 		
 		for (FieldMessage e : list) { //serve para percorrer a lista de FieldMessage, e p/ cada obj que tiver na lista vai ser add um 
 		//erro correspondente na lista de erros do framework que está descrito abaixo
